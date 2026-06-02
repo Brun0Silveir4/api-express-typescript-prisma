de0f47accf87ab7aeadb561cd99eeb64763fdc01
Brun0Silveir4 <bruno.silveira.dionisio@gmail.com>
2026-06-02 20:38:26 -0300

    fix: fixing mongoDB connection. Now using atlas

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,6 +1,7 @@
 import {Router} from 'express'
 import {register, login} from '../controllers/auth.js'
 import {protect} from '../middlewares/auth.js'
+import { prisma } from '../lib/prisma.js'
 
 const router = Router()
 
@@ -11,6 +12,16 @@ router.get('/me', protect, (req, res) => {
     res.json({message: 'Acesso autorizado!',
         user: req.user
     })
+})
+
+router.get('/perfil', protect, async (req, res) => {
+  const userId = req.user?.id
+  
+  const user = await prisma.user.findUnique({
+    where: { id: userId }
+  })
+
+  res.json(user)
 })
 
 export default router
