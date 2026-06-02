a5195763359133617e8ebf0a955c814830a39059
Brun0Silveir4 <bruno.silveira.dionisio@gmail.com>
2026-06-02 18:47:35 -0300

    minor fix

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,7 +1,7 @@
 import "dotenv/config";
 import process from "node:process";
 
-// Definimos o objeto de configuração diretamente exportando-o no formato exigido
+
 export default {
   schema: "prisma/schema.prisma",
   datasource: {
